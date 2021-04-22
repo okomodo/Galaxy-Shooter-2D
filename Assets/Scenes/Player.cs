bfe017ba64f52db3aa7d50ec25c266f441d76a81
kokomodo <ebris700@gmail.com>
2021-04-21 20:47:28 -0400

    added Character Movement

--- Assets/Scenes/Player.cs
+++ Assets/Scenes/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody _rigid;
+    [SerializeField] private int _speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0f, 1f, 0f);
+        _rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector3 _direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+        _rigid.MovePosition( _rigid.position + _direction * _speed * Time.fixedDeltaTime);
     }
 }
